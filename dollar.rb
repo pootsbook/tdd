aa9ab21602cb87d0b5ce95c63b59012d7dae1cbb
Josh Glover <jmglov@alhana.(none)>
2010-08-11 21:29:12 +0100

    Dollar#amount is protected (GB) (C4 - Privacy)

--- dollar.rb
+++ dollar.rb
@@ -1,6 +1,4 @@
 class Dollar
-  attr_reader :amount
-
   def initialize(amount)
     @amount = amount
   end
@@ -12,4 +10,7 @@ class Dollar
   def times(multiplier)
     Dollar.new(@amount * multiplier)
   end
+
+  protected
+  attr_reader :amount
 end
