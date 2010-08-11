58a537f7ced1f1cbc7e19540334ea2b4cea3dea3
Josh Glover <jmglov@alhana.(none)>
2010-08-11 21:57:04 +0100

    Added TestFranc#test_equality (GB) (C6 - Equality for All, Redux)

--- test_Franc.rb
+++ test_Franc.rb
@@ -9,4 +9,9 @@ class TestFranc < MiniTest::Unit::TestCase
     assert_equal(Franc.new(10), five.times(2))
     assert_equal(Franc.new(15), five.times(3))
   end
+
+  def test_equality
+    assert(Franc.new(5) == Franc.new(5))
+    refute(Franc.new(5) == Franc.new(6))
+  end
 end
