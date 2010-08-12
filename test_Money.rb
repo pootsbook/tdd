35a9c452a7a0a691cf81ddb9b9e42ab4176ad16f
Josh Glover <jmglov@alhana.(none)>
2010-08-12 12:27:50 +0100

    Added TestMoney#test_simple_addition (ERR) (C12 - Addition, Finally)

--- test_Money.rb
+++ test_Money.rb
@@ -20,4 +20,9 @@ class TestMoney < MiniTest::Unit::TestCase
     assert_equal(:USD, Money.dollar(1).currency)
     assert_equal(:CHF, Money.franc(1).currency)
   end
+
+  def test_simple_addition
+    sum = Money.dollar(5) + Money.dollar(5)
+    assert_equal(Money.dollar(10), sum)
+  end
 end
