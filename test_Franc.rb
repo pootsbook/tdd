811b11a690a84ba67f57f2eb1c1b3204fab11d57
Josh Glover <jmglov@alhana.(none)>
2010-08-12 11:42:40 +0100

    Removed TestMoney#test_dft_class_inequality (GB) (and TestFranc#test_equality, which is equivalent to removing the third and forth assertions in testEquality() from TDDbE p.52) (C11 - The Root of All Evil)

--- test_Franc.rb
+++ test_Franc.rb
@@ -9,9 +9,4 @@ class TestFranc < MiniTest::Unit::TestCase
     assert_equal(Money.franc(10), five.times(2))
     assert_equal(Money.franc(15), five.times(3))
   end
-
-  def test_equality
-    assert(Money.franc(5) == Money.franc(5))
-    refute(Money.franc(5) == Money.franc(6))
-  end
 end
