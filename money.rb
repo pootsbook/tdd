0ed898f80ff793cd7da357102577323bfe51a373
Josh Glover <jmglov@alhana.(none)>
2010-08-12 19:47:30 +0100

    Sum#reduce reduces both arguments (GB) (C15 - Mixed Currencies)

--- money.rb
+++ money.rb
@@ -64,7 +64,8 @@ class Sum
   end
 
   def reduce(bank, to_currency)
-    amount = @augend.amount + @addend.amount
+    amount = @augend.reduce(bank, to_currency).amount +
+      @addend.reduce(bank, to_currency).amount
     Money.new(amount, to_currency)
   end
 end
