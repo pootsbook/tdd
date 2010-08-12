d64c9cf8b7164f7996edf8f325684834682568e1
Josh Glover <jmglov@alhana.(none)>
2010-08-12 19:37:39 +0100

    Added TestMoney#test_mixed_addition (RB) (C15 - Mixed Currencies) (this would throw "a host of compile errors" in Java, according to TDDbE, but duck typing means we compile just fine and simply fail a test)

--- test_Money.rb
+++ test_Money.rb
@@ -59,4 +59,13 @@ class TestMoney < MiniTest::Unit::TestCase
   def test_identity_rate
     assert_equal(1, Bank.new.rate(:USD, :USD))
   end
+
+  def test_mixed_addition
+    five_bucks = Money.dollar(5)
+    ten_francs = Money.franc(10)
+    bank       = Bank.new
+    bank.add_rate(:CHF, :USD, 2)
+    result = bank.reduce(five_bucks + ten_francs, :USD)
+    assert_equal(Money.dollar(10), result)
+  end
 end
