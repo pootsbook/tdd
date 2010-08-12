3d783f820bcb0e4fd0ef351aedf112d4595f38ff
Josh Glover <jmglov@alhana.(none)>
2010-08-12 19:26:05 +0100

    Bank stores rates in hashtable (ERR) (C14 - Change) (because Ruby can use an array--or any other object, for that matter--as a hash key, we don't need the Pair class from TDDbEx; we can simply use a two-element array ([from_currency, to_currency]) as our rate hash key)

--- money.rb
+++ money.rb
@@ -34,15 +34,20 @@ class Money
 end
 
 class Bank
+  def initialize
+    @rates = {}
+  end
+
   def reduce(expression, to_currency)
     expression.reduce(self, to_currency)
   end
 
   def add_rate(from, to, rate)
+    @rates[[from, to]] = rate
   end
 
   def rate(from, to)
-    (from == :CHF && to == :USD) ? 2 : 1
+    @rates[[from, to]]
   end
 end
 
